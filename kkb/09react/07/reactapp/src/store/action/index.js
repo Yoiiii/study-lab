39d41fd9452cb9d65fc6107c891d942b5d1e4d9e
Yoiiii <majestyling@hotmail.com>
2021-03-30 17:39:51 +0800

    9-8 fin

--- kkb/09react/07/reactapp/src/store/action/index.js
+++ kkb/09react/07/reactapp/src/store/action/index.js
@@ -6,11 +6,9 @@ function getList({page=1,tab="all",limit=10,mdrender=false}){
     dispatch({
       type:'LIST_LOAD'
     })
-    //console.log(page,tab,limit);
     http.get(`/topics?${qs.stringify({page,tab,limit,mdrender})}`)
     .then((res)=>{
       if(res.data.success){
-        //console.log(res.data.data);
         dispatch({
           type:'LIST_LOADOVER',
           data:res.data.data
--- kkb/09react/07/reactapp/src/view/index/index.js
+++ kkb/09react/07/reactapp/src/view/index/index.js
@@ -5,23 +5,23 @@ import {useLocation,Link,useHistory} from 'react-router-dom'
 import {useSelector,useDispatch} from 'react-redux'
 import {getList} from '../../store/action/index';
 import qs from 'qs'
-//parse : tab=share&page=2 转成 {tab:"share",page:2}
-//stringify:{tab:"share",page:2} 转成  tab=share&page=2 
 function Index() {
   let {loading,data}=useSelector(state=>state.list)
   let dispatch=useDispatch()
   let {search} =useLocation()
   search =qs.parse(search.substr(1))
   let history = useHistory()
-  let {page=1,tab="all"}=search
+  let {page=1,tab="all",limit="10"}=search
+
   useEffect(()=>{
     dispatch(getList(search))
-  },[tab,page])
+  },[tab,page,limit])
+
   return (
     <div>
+      <IndexMenu/>
       <Layout.Content className="main">
         <List
-          header={<IndexMenu/>}
           dataSource={data}
           loading={loading}
           renderItem={item =>{
@@ -31,27 +31,16 @@ function Index() {
               </List.Item>
             )
           }}
-          // pagination={{
-          //   onChange:page=>{
-          //     console.log(page);
-          //   },
-          //   pageSize:10,//一页显示多少条
-          // }}
           footer={
           <Pagination 
             total={1000}
-            showTotal={(total,range)=>{
-              if(range[0] !== Number(page)){
-                let query = qs.stringify({...search})
-                query= qs.parse(query)
-                query.page=range[0]
-                console.log(search);
-                console.log(qs.stringify(query));
-                history.push('/?'+qs.stringify(query))
-              }
-              return `${range[0]}-${range[1]} of ${total} items`
+            onChange={(page, pageSize)=>{
+              console.log(page, pageSize);
+              search.page=page
+              search.limit=pageSize
+              history.push('/?'+qs.stringify(search))
             }}
-            pageSize={1}
+            pageSize={limit}
             defaultCurrent={Number(page)}
 
              />}
