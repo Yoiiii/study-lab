6c3dd025f418d3193491a40adcd0757e2f2600af
Yoiiii <majestyling@hotmail.com>
2021-04-02 17:19:31 +0800

    10-01-8 fin

--- kkb/10react实战/hook-app/src/todos/header.js
+++ kkb/10react实战/hook-app/src/todos/header.js
@@ -1,9 +1,29 @@
-import React from 'react'
-function Header(){
+import React, { useState } from 'react'
+
+function Header(props){
+  const [todo,setTodo]=useState("")
+  let {addTodo}=props
   return (<header id="create-todo">
     <h1>Todos</h1>
-    <input  type="text" placeholder="what needs to be done?"
-    value=""></input>
+    <input  
+      type="text"
+      placeholder="what needs to be done?"
+      value={todo}
+      onChange={(e)=>{
+        setTodo(e.target.value)
+      }}
+      onKeyDown={(e)=>{
+        if(e.keyCode===13){
+          if(!todo.trim()){
+            alert("请输入内容")
+            e.target.focus()
+          }else{
+            addTodo(todo)
+            setTodo("")
+          }
+        }
+      }}
+      />
   </header>)
 }
 export default Header
--- kkb/10react实战/hook-app/src/todos/index.js
+++ kkb/10react实战/hook-app/src/todos/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Header from './header'
 import Main from './main'
 import Footer from './footer'
 import './index.css'
 
 function Todos(){
+  const [todos,setTodos]=useState([])
+  function addTodo(val){
+    setTodos([...todos,{
+      id:Date.now(),
+      val,
+      completed:false
+    }])
+  }
   return (<div id="todoapp">
-    <Header/>
+    <Header addTodo={addTodo}/>
     <Main/>
     <Footer/>
   </div>)
