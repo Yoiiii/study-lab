871c996aebeee30959959795640ae3ff951f78a9
Yoiiii <majestyling@hotmail.com>
2021-04-14 17:24:59 +0800

    10-04-01 fin

--- kkb/10react实战/react-app/src/common/component/menu.js
+++ kkb/10react实战/react-app/src/common/component/menu.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { nav } from '../../router/route_list'
 
-export default function Menu(){
+export default function Menu(props) {
+  const { menuHide } = props
   return (
     <nav id="menu">
-      <a>首页</a>
-      <a>课程安排</a>
-      <a>讲师团队</a>
+      {nav.map((item, index) => {
+        return <NavLink 
+            key={index} 
+            className={item.className} 
+            to={item.path} 
+            exact={item.exact}
+            activeClassName="active"
+            onTouchEnd={() => {
+            menuHide()
+        }}>{item.name}</NavLink>
+      })}
     </nav>
   )
-}
+} 
