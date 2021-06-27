3a6611ad7d3a9e2ad6458b3300fba8450db9a762
Yoiiii <majestyling@hotmail.com>
2021-06-27 23:06:16 +0800

    13-02 fin

--- kkb/13webpack/02/devServer-demo/webpack.config.js
+++ kkb/13webpack/02/devServer-demo/webpack.config.js
@@ -26,6 +26,20 @@ module.exports={
   	// 自动开启浏览器
   	open: true,
   	// 端口
-  	port: 8081
+  	port: 8081,
+    //当我们的每次请求都会被devserver拦截，然后如果请求的url满足proxy中的某个规则的话，则把这个请求转发到另一个服务器上
+    //开启热更新
+    hot:true,
+
+    hotOnly:true,
+    proxy:{
+      '/api':{
+        target:'http://localhost:8888',
+        //把请求路径中的/api去掉
+        pathRewrite:{
+          '^/api/':''
+        }
+      }
+    }
 	}
 }
